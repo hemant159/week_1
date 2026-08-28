04a5ee35ff6297cac05102a8a388c79b5764d94e
suryanarayan-07 <ssurya9948@gmail.com>
2026-08-28 12:13:30 +0530

    Fixed the Small Bug which is change the = to === by surya

--- javascript/bug1.js
+++ javascript/bug1.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.url = '/') {
+  if (req.url === '/') {      //if the we use = instead of ===, it will not work as expected because = is an assignment operator, not a comparison operator. The correct operator to use for comparison is ===, which checks for both value and type equality.
     res.statusCode = 200;
     res.end('Hello World');
   } else {
