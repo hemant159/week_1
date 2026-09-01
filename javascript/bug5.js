ab11667dff6d95c6a786ffbf5d54b7b4fc7efe2e
Anish-SE <ranjusingh@gmail.com>
2026-09-01 12:03:19 +0530

    Fixed 'about' to '/about' in app.get()

--- javascript/bug5.js
+++ javascript/bug5.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('about', (req, res) => {
+// app.get('about', (req, res) => {
+//   res.send('This is the about page');
+// });
+
+app.get('/about', (req, res) => {
   res.send('This is the about page');
 });
 
