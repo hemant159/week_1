46d40cd20c07912fcf9f29849e38bb59e483ff66
Aman-das-dev <sahebprasaddas12@gmail.com>
2026-09-01 12:13:46 +0530

    fix bug2 HTTP response

--- javascript/bug2.js
+++ javascript/bug2.js
@@ -1,14 +1,18 @@
 const http = require('http');
 
 function requestHandler(req, res) {
-  if (req.url === '/') {
-    res.statusCode = 200;
-    console.log('Sending Hello World');
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
+    if (req.url === '/') {
+        res.statusCode = 200;
+        console.log('Sending Hello World');
+        res.end('Hello World');
+    } else {
+        res.statusCode = 404;
+        res.end('Not Found');
+    }
 }
 
 const server = http.createServer(requestHandler);
-server.listen(3000, () => console.log('Server running on port 3000'));
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
